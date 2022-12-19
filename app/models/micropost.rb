93691222ea3eb132df51e777a8ef7df49986cdf2
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-12-19 20:32:51 +0000

    Microposts validation and association with users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,7 @@
 class Micropost < ApplicationRecord
+  # List of validation helpers can be found here: 
+  # https://guides.rubyonrails.org/active_record_validations.html#validation-helpers
+  validates :content, length: { maximum: 140 }
+  
+  belongs_to :user
 end
