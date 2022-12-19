2a23285657b366cbaa974444cad56f6c09597baf
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-12-19 22:34:50 +0000

    Add validation for presence of micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,8 @@
 class Micropost < ApplicationRecord
   # List of validation helpers can be found here: 
   # https://guides.rubyonrails.org/active_record_validations.html#validation-helpers
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },
+                      presence: true
   
   belongs_to :user
 end
